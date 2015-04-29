bc0df418224718dc7efb9135ff9ad7c0e4c4893d
Tony Huang <tony.huang.jun@gmail.com>
2015-04-29 01:44:38 -0400

    added rudimentary friends list

--- android/WhereYou/src/com/tonyjhuang/whereyou/WhereYouStarterActivity.java
+++ android/WhereYou/src/com/tonyjhuang/whereyou/WhereYouStarterActivity.java
@@ -5,20 +5,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.FunctionCallback;
 import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseCloud;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
 import com.parse.ParseQuery;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -32,6 +43,12 @@ public class WhereYouStarterActivity extends Activity {
 	EditText usernameInput;
 	@InjectView(R.id.target_input)
 	EditText targetInput;
+	@InjectView(R.id.friend_input)
+	EditText friendInput;
+	@InjectView(R.id.friends)
+	ListView friendsListView;
+
+	private FriendsListAdapter friendsAdapter;
 
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +60,9 @@ public class WhereYouStarterActivity extends Activity {
 
 		ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
 		setNameView(currentInstallation.getString("name"));
+		friendsAdapter = new FriendsListAdapter(currentInstallation.getJSONArray("friends"));
+		friendsListView.setAdapter(friendsAdapter);
+
 	}
 
 	@OnClick(R.id.username_save)
@@ -67,13 +87,125 @@ public class WhereYouStarterActivity extends Activity {
 
 	@OnClick(R.id.target_push)
 	public void onTargetPushClick(View view) {
-		String targetName = targetInput.getText().toString();
+		poke(targetInput.getText().toString());
+	}
+
+	private void poke(String name) {
 		ParseQuery<ParseInstallation> pushQuery = ParseInstallation.getQuery();
-		pushQuery.whereEqualTo("name", targetName);
+		pushQuery.whereEqualTo("name", name);
 
 		ParsePush push = new ParsePush();
 		push.setQuery(pushQuery);
 		push.setMessage("Giants scored against the A's! It's now 2-2.");
 		push.sendInBackground();
+	}
+
+	@OnClick(R.id.friend_add)
+	public void onFriendAddClick(View view) {
+		final String newFriend = friendInput.getText().toString();
+		final ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
+		JSONArray friendsList = currentInstallation.getJSONArray("friends");
+		if(friendsList != null) {
+			for(int i = 0; i < friendsList.length(); i++) {
+				try {
+					if (newFriend.equals(friendsList.getString(i))) {
+						showToast(newFriend + " is already on your friends list");
+						return;
+					}
+				} catch(JSONException e) {
+					Log.e("Main", e.getMessage());
+					showToast("Sorry, hit an error. Try again?");
+				}
+			}
+		}
+
+		checkName(newFriend, new FunctionCallback<Boolean>() {
+			@Override
+			public void done(Boolean nameExists, ParseException e) {
+				if (e == null) {
+					if (nameExists) {
+						// Update current installation friends list with new friend.
+						JSONArray friendsList = currentInstallation.getJSONArray("friends");
+						if (friendsList == null) friendsList = new JSONArray();
+						friendsList.put(newFriend);
+						currentInstallation.put("friends", friendsList);
+						currentInstallation.saveInBackground();
+
+						friendsAdapter.update(friendsList);
+					} else {
+						showToast("Yo, no one exists with that name!");
+					}
+				} else {
+					Log.e("Main", e.getMessage());
+					showToast(e.getMessage());
+				}
+			}
+		});
+	}
+
+	private void checkName(String name, FunctionCallback<Boolean> callback) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("name", name);
+		ParseCloud.callFunctionInBackground("checkName", params, callback);
+	}
+
+	private Toast currentToast;
+	private void showToast(String msg) {
+		if(currentToast == null) currentToast.cancel();
+		currentToast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+		currentToast.show();
+	}
+
+	class FriendsListAdapter extends BaseAdapter {
+
+		private JSONArray friends;
+
+		public FriendsListAdapter(JSONArray friends) {
+			this.friends = friends;
+		}
+
+		public void update(JSONArray friends) {
+			this.friends = friends;
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return friends != null ? friends.length() : 0;
+		}
+
+		@Override
+		public String getItem(int i) {
+			try {
+				return friends != null ? friends.getString(i) : null;
+			} catch (JSONException e) {
+				return null;
+			}
+
+		}
+
+		@Override
+		public long getItemId(int i) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int i, View view, ViewGroup viewGroup) {
+			if(view == null) {
+				view = new TextView(WhereYouStarterActivity.this);
+			}
+
+			final String friend = getItem(i);
+			((TextView) view).setText(friend);
+			view.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					showToast("poke!");
+					poke(friend);
+				}
+			});
+
+			return view;
+		}
 	}
 }
