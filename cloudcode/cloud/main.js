e4ac49d13de469b0a65d581d4fd173c0a83de242
Tony Huang <tony.huang.jun@gmail.com>
2015-04-29 01:44:29 -0400

    added checkName function

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -4,3 +4,21 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("checkName", function(request, response) {
+  Parse.Cloud.useMasterKey(); 
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("name", request.params.name);
+  query.count({
+        success: function(count){
+            if(count > 0) {
+              response.success(true);
+            } else {
+              response.success(false);
+            }
+        },
+        error: function(error){
+            response.error(error);
+        }       
+    });
+});
