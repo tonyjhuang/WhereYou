19e8b04f25350f0312ea87668172d8d0cda2f7e4
Tony Huang <tony.huang.jun@gmail.com>
2015-04-29 21:35:33 -0400

    created new view for friends list, moved more functionality into parse helper

--- android/WhereYou/src/main/java/com/tonyjhuang/whereyou/MainActivity.java
+++ android/WhereYou/src/main/java/com/tonyjhuang/whereyou/MainActivity.java
@@ -5,27 +5,17 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import com.parse.FunctionCallback;
-import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
-import com.parse.ParseException;
 import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-import com.parse.ParsePush;
-import com.parse.ParseQuery;
 import com.tonyjhuang.whereyou.api.ParseHelper;
 import com.tonyjhuang.whereyou.helpers.AppRouter;
 import com.tonyjhuang.whereyou.helpers.WhereYouActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -43,9 +33,8 @@ public class MainActivity extends WhereYouActivity {
     @InjectView(R.id.location)
     TextView locationText;
     @InjectView(R.id.friends)
-    ListView friendsListView;
+    FriendsListView friendsListView;
 
-    private FriendsListAdapter friendsAdapter;
     private ParseHelper parseHelper = new ParseHelper();
 
     public void onCreate(Bundle savedInstanceState) {
@@ -61,8 +50,7 @@ public class MainActivity extends WhereYouActivity {
             setContentView(R.layout.activity_main);
 
             setNameView(name);
-            friendsAdapter = new FriendsListAdapter(currentInstallation.getJSONArray("friends"));
-            friendsListView.setAdapter(friendsAdapter);
+            friendsListView.setFriends(currentInstallation.getJSONArray("friends"));
         }
     }
 
@@ -73,43 +61,17 @@ public class MainActivity extends WhereYouActivity {
         parseHelper.updateName(newName);
     }
 
-    private void getInstallation(String name, GetCallback<ParseObject> callback) {
-        ParseQuery<ParseObject> query = new ParseQuery<>("Installation");
-        query.whereEqualTo("name", name);
-        query.getFirstInBackground(callback);
-    }
-
     private void setNameView(String name) {
         helloText.setText("Hello " + name + "!");
     }
 
-    private void poke(String name) {
-        ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
-        String myName = currentInstallation.getString("name");
-
-        ParseQuery<ParseInstallation> pushQuery = ParseInstallation.getQuery();
-        pushQuery.whereEqualTo("name", name);
-
-        JSONObject data = new JSONObject();
-        try {
-            data.put("name", myName);
-            data.put("alert", myName + " wants to know where you at! Tap here to share your location.");
-            data.put("action", "com.tonyjhuang.whereyou.ASK");
-        } catch (JSONException e) {
-            Log.e("Main", e.getMessage());
-        }
-
-        ParsePush push = new ParsePush();
-        push.setQuery(pushQuery);
-        push.setData(data);
-        push.sendInBackground();
-    }
 
     @OnClick(R.id.friend_add)
     public void onFriendAddClick(View view) {
         final String newFriend = friendInput.getText().toString();
-        final ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
-        JSONArray friendsList = currentInstallation.getJSONArray("friends");
+        JSONArray friendsList = ParseInstallation.getCurrentInstallation().getJSONArray("friends");
+
+        // Does the user already have this guy on his friends list?
         if (friendsList != null) {
             for (int i = 0; i < friendsList.length(); i++) {
                 try {
@@ -119,30 +81,23 @@ public class MainActivity extends WhereYouActivity {
                     }
                 } catch (JSONException e) {
                     Log.e("Main", e.getMessage());
-                    showToast("Sorry, hit an error. Try again?");
                 }
             }
         }
 
-        parseHelper.checkName(newFriend, new FunctionCallback<Boolean>() {
+        parseHelper.addFriend(newFriend, new ParseHelper.Callback<JSONArray>() {
             @Override
-            public void done(Boolean nameExists, ParseException e) {
-                if (e == null) {
-                    if (nameExists) {
-                        // Update current installation friends list with new friend.
-                        JSONArray friendsList = currentInstallation.getJSONArray("friends");
-                        if (friendsList == null) friendsList = new JSONArray();
-                        friendsList.put(newFriend);
-                        currentInstallation.put("friends", friendsList);
-                        currentInstallation.saveInBackground();
+            public void onFinish(JSONArray friendsList) {
+                friendsListView.setFriends(friendsList);
+            }
 
-                        friendsAdapter.update(friendsList);
-                    } else {
-                        showToast("Yo, no one exists with that name!");
-                    }
+            @Override
+            public void onError(Throwable e) {
+                if (e.getMessage().equals("No such user")) {
+                    showToast("Yo, no one exists with that name!");
                 } else {
                     Log.e("Main", e.getMessage());
-                    showToast(e.getMessage());
+                    showToast("UHHHHH SOMETHING WENT WRONG. TRY AGAIN?????");
                 }
             }
         });
@@ -161,58 +116,5 @@ public class MainActivity extends WhereYouActivity {
                         locationText.setText(loc);
                     }
                 });
-    }
-
-    class FriendsListAdapter extends BaseAdapter {
-
-        private JSONArray friends;
-
-        public FriendsListAdapter(JSONArray friends) {
-            this.friends = friends;
-        }
-
-        public void update(JSONArray friends) {
-            this.friends = friends;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            return friends != null ? friends.length() : 0;
-        }
-
-        @Override
-        public String getItem(int i) {
-            try {
-                return friends != null ? friends.getString(i) : null;
-            } catch (JSONException e) {
-                return null;
-            }
-
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            if (view == null) {
-                view = new TextView(MainActivity.this);
-            }
-
-            final String friend = getItem(i);
-            ((TextView) view).setText(friend);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    showToast("poke!");
-                    poke(friend);
-                }
-            });
-
-            return view;
-        }
     }
 }
--- android/WhereYou/src/main/java/com/tonyjhuang/whereyou/api/ParseHelper.java
+++ android/WhereYou/src/main/java/com/tonyjhuang/whereyou/api/ParseHelper.java
@@ -1,8 +1,18 @@
 package com.tonyjhuang.whereyou.api;
 
+import android.util.Log;
+
 import com.parse.FunctionCallback;
 import com.parse.ParseCloud;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
+import com.tonyjhuang.whereyou.services.WhereYouAction;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,5 +33,59 @@ public class ParseHelper {
         currentInstallation.put("name", name);
         currentInstallation.put("nameLowercase", name.toLowerCase());
         currentInstallation.saveInBackground();
+    }
+
+    public void addFriend(final String name, final Callback<JSONArray> callback) {
+        checkName(name, new FunctionCallback<Boolean>() {
+            @Override
+            public void done(Boolean nameExists, ParseException e) {
+                if (e == null) {
+                    if (nameExists) {
+                        // Update current installation friends list with new friend.
+                        ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
+                        JSONArray friendsList = currentInstallation.getJSONArray("friends");
+                        if (friendsList == null) friendsList = new JSONArray();
+                        friendsList.put(name);
+                        currentInstallation.put("friends", friendsList);
+                        currentInstallation.saveInBackground();
+
+                        callback.onFinish(friendsList);
+                    } else {
+                        callback.onError(new Error("No such user"));
+                    }
+                } else {
+                    Log.e("ParseHelper", e.getMessage());
+                    callback.onError(e);
+                }
+            }
+        });
+    }
+
+    public void poke(String name) {
+        ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
+        String myName = currentInstallation.getString("name");
+
+        ParseQuery<ParseInstallation> pushQuery = ParseInstallation.getQuery();
+        pushQuery.whereEqualTo("name", name);
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("name", myName);
+            data.put("alert", myName + " wants to know where you at! Tap here to share your location.");
+            data.put("action", WhereYouAction.ASK);
+        } catch (JSONException e) {
+            Log.e("ParseHelper", e.getMessage());
+        }
+
+        ParsePush push = new ParsePush();
+        push.setQuery(pushQuery);
+        push.setData(data);
+        push.sendInBackground();
+    }
+
+    public interface Callback<T> {
+        void onFinish(T t);
+
+        void onError(Throwable e);
     }
 }
